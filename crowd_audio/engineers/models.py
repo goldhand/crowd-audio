6a22778ea2ae66e0374a1e88febb8b9d0b73e267
Will Farley <will@django.nu>
2014-02-09 16:41:25 -0700

    adds engineer models

--- crowd_audio/engineers/models.py
+++ crowd_audio/engineers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Engineer(models.Model):
+	name = models.CharField(max_length=255)
+	user = models.ForeignKey('users.User', related_name="engineers")
+
+	def __unicode__(self):
+		return self.name
