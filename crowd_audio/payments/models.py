7ba2ee38cdcee38bb2708174373ee255a6ec7d95
Will Farley <will@django.nu>
2014-02-09 16:41:37 -0700

    adds payment models

--- crowd_audio/payments/models.py
+++ crowd_audio/payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+	competition = models.ForeignKey("competitions.Competition", related_name="payments")
+	user = models.ForeignKey("users.User", related_name="payments")
+	transaction = models.OneToOneField("transactions.Transaction")
+	amount = models.DecimalField(decimal_places=2, max_digits=7)
+
+	def __unicode__(self):
+		return self.amount
